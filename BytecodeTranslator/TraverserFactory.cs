827d8c83f069b077768b42cf37737129e0522a24
Mike Barnett <mbarnett@microsoft.com>
2011-08-01 16:09:18 -0700

    Fix in the assertion injector for putting the output on top of the input assembly. Fix in the Sink so that the consolidated index of a type parameter is computed correctly.

---
+++ BytecodeTranslator/TraverserFactory.cs
@@ -0,0 +1,37 @@
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Cci;
+using Microsoft.Cci.MetadataReader;
+using Microsoft.Cci.MutableCodeModel;
+using Microsoft.Cci.Contracts;
+using Microsoft.Cci.ILToCodeModel;
+
+using TranslationPlugins;
+
+using Bpl = Microsoft.Boogie;
+
+namespace BytecodeTranslator {
+  public abstract class TraverserFactory {
+    public virtual MetadataTraverser MakeMetadataTraverser(Sink sink,
+      IDictionary<IUnit, IContractProvider> contractProviders, // TODO: remove this parameter?
+      IDictionary<IUnit, PdbReader> sourceLocationProviders)
+    {
+      return new MetadataTraverser(sink, sourceLocationProviders);
+    }
+    public virtual StatementTraverser MakeStatementTraverser(Sink sink, PdbReader/*?*/ pdbReader, bool contractContext) {
+      return new StatementTraverser(sink, pdbReader, contractContext);
+    }
+
+    public virtual ExpressionTraverser MakeExpressionTraverser(Sink sink, StatementTraverser/*?*/ statementTraverser, bool contractContext) {
+      return new ExpressionTraverser(sink, statementTraverser, contractContext);
+    }
+  }
+}
